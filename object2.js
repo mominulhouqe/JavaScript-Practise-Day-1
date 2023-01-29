6c40200c45e27b6b120892b02b6e28a372a9facc
Mominul Hoque <88490184+Momin69@users.noreply.github.com>
2023-01-30 01:04:53 +0600

    object-2

---
+++ object2.js
@@ -0,0 +1,12 @@
+
+let students = {
+    Name : "Mominul Houqe",
+    age : 21,
+    Semester : "6th",
+    Roll : 476765
+}
+
+let studentsKays = Object.keys(students);
+let studentValues = Object.values(students);
+console.log(studentsKays);
+console.log(studentValues);
