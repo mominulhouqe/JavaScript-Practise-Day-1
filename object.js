2f1187cb8677969f3087683a9da6164733655444
Mominul Hoque <88490184+Momin69@users.noreply.github.com>
2023-01-30 00:02:49 +0600

    adding object declare

---
+++ object.js
@@ -0,0 +1,8 @@
+let students = {
+    Name : "Mominul Houqe",
+    age : 21,
+    Semester : "6th",
+    Roll : 476765
+}
+console.log("Student Information = ", students);
+console.log("My Semester is = " , students.Semester);
